a829bc346957b8a460a8e42032c7cedba76a7765
Rahul <87780553+Rahulcopico@users.noreply.github.com>
2023-12-02 23:07:02 +0530

    Add files via upload

---
+++ node/config.js
@@ -0,0 +1,11 @@
+
+var MID = "XLfMwz77755817306555";
+var MKEY = "QK2YbRqde@TPwK_r";
+
+var ENV= 'securegw-stage.paytm.in';
+var WEBSITE= 'WEBSTAGING';
+
+exports.MID = MID;
+exports.MKEY = MKEY;
+exports.ENV = ENV;
+exports.WEBSITE = WEBSITE;
